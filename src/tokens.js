b215a94bee6032b8a831621fa438ab9781e2b7a0
Mehul Wagde <yoinkingfish@gmail.com>
2026-09-22 01:40:40 +0530

    fix: export revocation head helper

--- src/tokens.js
+++ src/tokens.js
@@ -375,4 +375,4 @@ function addRevocation({ type, target, reason, org_id, kid = null }) {
   return rec;
 }
 
-module.exports = { rid: require('./crypto').rid, createOrgRecord, publicOrg, orgPubkey, issuePassport, publicPassport, rotatePassport, setPassportStatus, revokeKey, touchLastSeen, passportStatus, LIFECYCLE, createBlueprint, blueprintInstances, isOrgLocked, assertOrgUsable, assertPassportUsable, keyFor, registerDelegation, assertTokenUsable, tokenCovers, checkBudget, debitBudget, allowCustody, isPassportRevoked, addRevocation };
+module.exports = { rid: require('./crypto').rid, createOrgRecord, publicOrg, orgPubkey, issuePassport, publicPassport, rotatePassport, setPassportStatus, revokeKey, touchLastSeen, passportStatus, LIFECYCLE, createBlueprint, blueprintInstances, isOrgLocked, assertOrgUsable, assertPassportUsable, keyFor, registerDelegation, assertTokenUsable, tokenCovers, checkBudget, debitBudget, allowCustody, isPassportRevoked, addRevocation, revocationHead };
